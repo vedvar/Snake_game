e3cfdc8a28188f929a01a41c3d9ae9ec201c3503
Ved Varshney <vedvarshney@gmail.com>
2023-12-01 13:53:14 -0800

    Added template

---
+++ part2.py
@@ -0,0 +1,18 @@
+# Group #: G36
+# Student names: Ved Varshney, Shawn Nagra
+ 
+import threading
+import queue
+import time, random
+
+def consumerWorker (queue):
+    """target worker for a consumer thread"""
+    # to implement  
+  
+def producerWorker(queue):
+    """target worker for a producer thread"""
+    # to implement
+
+if __name__ == "__main__":
+    buffer = queue.Queue()
+    # to implement
